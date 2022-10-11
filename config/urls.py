a3e31fbc0f950d62687d6dcc16c4241dd072cfb0
eraliabdinazarov <eraliabdinazarov22@gmail.com>
2022-10-11 14:07:56 +0500

    commit files

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('authentication.urls', namespace='authentication')),
+    path('auth/', include('authentication.urls', namespace='authentication')),
+    path('', include('fulfil.urls', namespace='fulfil')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
