023eb0a3d7746f225ef0d675ed6cd3ebbbbe6986
eraliabdinazarov <eraliabdinazarov22@gmail.com>
2022-10-19 22:47:25 +0500

    commit and push files

--- fulfil/urls.py
+++ fulfil/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, success
 app_name = 'fulfil'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('success/', success, name='success'),
 ]
